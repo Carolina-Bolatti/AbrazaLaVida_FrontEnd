737f13e760f7d2ce28dad349ecc1fb196e29e61a
Monica <pmonicavivian@gmail.com>
2022-10-28 14:57:58 -0300

    Arreglos Varios

--- JS/contacto.js
+++ JS/contacto.js
@@ -23,4 +23,50 @@ async function handleSubmit(event) {
     }
 }
     
+function valida_envia() {
+  
+    if (document.fvalida.firstname.value.length == 0){
+        alert("Tiene que escribir su nombre");
+        document.fvalida.firstname.focus()
+        return 0;
+    
+    }else if (document.fvalida.lastname.value.length == 0){
+      alert("Tiene que escribir su apellido");
+      document.fvalida.lastname.focus()
+      return 0;
+     
+    }else if (document.fvalida.lastname.value.length == 0){
+        alert("Tiene que escribir su mail");
+        document.fvalida.lastname.focus()
+        return 0;  
+
+    }else if (document.fvalida.lastname.value.length == 0){
+        alert("Tiene que escribir su mensaje");
+        document.fvalida.lastname.focus()
+        return 0;     
+
+    }else {
+    alert("Gracias por enviar el formulario")
+    document.fvalida.submit(); 
+    }
+  
+}
+
+function validarEntero(valor) {
+
+valor = parseInt(valor)
+
+if (isNaN(valor)) {
+  
+  return ""
+} else {
+  
+  return valor
+}
+
+}
+
+
+
+
 
